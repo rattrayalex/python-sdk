be6ac8e133b7456bc167c158323da0edb74cdd1d
Dmitry Paramonov <asmatic075@gmail.com>
2024-10-29 09:52:39 +0300

    fix: Extend create and create_or_update methods for tasks

--- src/julep/__init__.py
+++ src/julep/__init__.py
@@ -1,6 +1,7 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 from . import types
+from .lib import setup
 from ._types import NOT_GIVEN, NoneType, NotGiven, Transport, ProxiesTypes
 from ._utils import file_from_path
 from ._client import (
@@ -37,6 +38,8 @@ from ._exceptions import (
 )
 from ._base_client import DefaultHttpxClient, DefaultAsyncHttpxClient
 from ._utils._logs import setup_logging as _setup_logging
+
+setup()
 
 __all__ = [
     "types",
